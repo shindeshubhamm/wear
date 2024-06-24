4a4b97dfd22f6d64ec6c3ae49621727c864ddfd9
Somesh-Salunkhe <salunkhesomesh1602@gmail.com>
2024-06-24 10:54:06 +0200

    Update __init__.py

--- augmentation/__init__.py
+++ augmentation/__init__.py
@@ -4,7 +4,7 @@ Augmentation Module
 """
 
 
-augmentation_choices = ['autoaughar', 'cutmix', 'jittering', 'mixup', 's1', 's2', 's3', 's4']
+augmentation_choices = ['autoaughar', 'cutmix', 'jittering', 'mixup', 's1', 's2', 's3', 's4',, 'js1','js2']
 """
 List of valid augmentation choices
 
@@ -16,6 +16,8 @@ Valid choices for augmentation techniques:
 - 's2': Augment leg modality using cutmix.
 - 's3': Augment arm modality using mixup.
 - 's4': Augment leg modality using mixup.
+- 'js1': Augment arm modality only using jitter.
+- 'js2': Augment leg modality only using jitter.
 """
 
 from .augment import apply_augmentation
